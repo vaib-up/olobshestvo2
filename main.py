d27ccf171b3086aad32fde7a9b78c491e0a47a90
vaib-up <burasnikovegor16@gmail.com>
2026-05-29 20:19:31 +0300

    wrong answer 3.0

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def get_main_keyboard():
             [KeyboardButton(text="🔁 Старт"), KeyboardButton(text="📈 Статистика")],
             [KeyboardButton(
                 text="🤖 Помощник",
-                web_app=WebAppInfo(url="https://olobshestvo2.online/miniapp/index.html")
+                web_app=WebAppInfo(url="https://olobshestvo2.online/miniapp/index.html?v=2")
             )],
         ],
         resize_keyboard=True,
